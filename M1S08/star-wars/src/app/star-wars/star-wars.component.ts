24b7e86dcb2f87fa3f099fb4d617d2bdedfaf12e
marcosev <marcosev@gmail.com>
2023-03-15 23:52:47 -0300

    Update star-wars.component.ts

--- M1S08/star-wars/src/app/star-wars/star-wars.component.ts
+++ M1S08/star-wars/src/app/star-wars/star-wars.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { PersonagemClass } from '../personagem-class';
+import { PersonagensStarWarsService } from '../personagens-star-wars.service';
 
 @Component({
   selector: 'app-star-wars',
   templateUrl: './star-wars.component.html',
   styleUrls: ['./star-wars.component.css']
 })
-export class StarWarsComponent {
+export class StarWarsComponent implements onInit {
 
-}
+  public personagens: PersonagemClass[] = [];
+  constructor(private_service: PersonagensStarWarsService){}
+
+  ngOninit():void{
+    this._service.getPersonagens().subscribe(
+      (res) => {
+        this.personagens = res
+      }
+      )
+   
+  }
+
+  public getPersonagensFromService({
+
+  })
+
+  
+
+
+  }
+
+
