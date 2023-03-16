21863362e3eb7605a713dc5612ffe2c186bdd7de
marcosev <marcosev@gmail.com>
2023-03-15 23:33:59 -0300

    Update personagens-star-wars.service.ts

--- M1S08/star-wars/src/app/personagens-star-wars.service.ts
+++ M1S08/star-wars/src/app/personagens-star-wars.service.ts
@@ -1,12 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { API_PATH } from '../environments/environments'
+import { PersonagemClass } from './personagem-class';
 @Injectable({
   providedIn: 'root'
 })
 
 export class PersonagensStarWarsService {
   
-  private url ="http://localhost:3000/personagens";
+  private url = "http://localhost:3000/personagens";
+  private urlEnv = ""
   constructor(private_httpClient: HttpClient) { }
+}
+
+
+public getPersonagens() {
+  return this.HttpClient.get<PersonagemClass[]>(`${API_PATH}Personagens`)
 }
